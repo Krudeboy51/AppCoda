701455dc442b6a2dd097c87b0644be01ebaa8b39
Krudeboy51 <kking9351@gmail.com>
2016-10-19 12:41:54 -0400

    second commit xcode v.7.3

---
+++ AppCodaAssignment/XMLParser.swift
@@ -0,0 +1,13 @@
+//
+//  XMLParser.swift
+//  AppCodaAssignment
+//
+//  Created by Kory E King on 10/19/16.
+//  Copyright © 2016 Kory E King. All rights reserved.
+//
+
+import UIKit
+
+class XMLParser: NSObject {
+
+}
